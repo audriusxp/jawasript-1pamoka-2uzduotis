3d40b2364d304e23adebe0112e71abfef1fb6768
audriusxp <palm@inbox.lt>
2022-06-08 08:48:14 +0300

    turio skaciuokle

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    let plotis;
+    let ilgis;
+    let aukstis;
+
+    let turis;
+
+    ilgis = 15;
+    plotis = 20;
+    aukstis= 5;
+    turis = staciakampioTuris(ilgis,plotis,aukstis);
+    console.log(turis);
+}
+
+    function staciakampioTuris(ilgis,plotis,aukstis) {
+        return a*b*c;
+    }
